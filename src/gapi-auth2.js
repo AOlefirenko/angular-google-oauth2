f992dc05db0c0450239cbeed2c49fa0003cd6495
Andrey Olefirenko <ole.olefirenko@gmail.com>
2016-04-30 17:01:22 +0300

    add wrapper isSignIn

--- src/gapi-auth2.js
+++ src/gapi-auth2.js
@@ -4,28 +4,44 @@ export default function gapiAuth2($window, $q) {
   var svc = this;
   var deferred = $q.defer();
   var auth2Promise = deferred.promise;
-  $window.gapi.load('auth2', ()=> {
+  
+  $window.gapi.load('auth2', () => {
     deferred.resolve($window.gapi.auth2)
   });
 
 
-  svc.getAuthInstance = function () {
+  svc.getAuthInstance = function() {
     var deferred = $q.defer();
-    auth2Promise.then((auth2)=> {
+    auth2Promise.then((auth2) => {
       deferred.resolve({
-        instance:auth2.getAuthInstance()
+        instance: auth2.getAuthInstance()
       });
     });
     return deferred.promise;
   }
 
-  svc.logout = function () {
+  svc.logout = function() {
     var deferred = $q.defer();
-    svc.getAuthInstance().then((res)=>{
-      res.instance.signOut().then(function () {
+    svc.getAuthInstance().then((res) => {
+      res.instance.signOut().then(function() {
         deferred.resolve();
       });
     })
     return deferred.promise;
   }
+
+  svc.isSignedIn = function() {
+    var deferred = $q.defer();
+    svc.getAuthInstance().then(x => {
+      var inst =  x.instance;
+      if (inst.currentUser) {
+        return deferred.resolve(inst.currentUser.get().isSignedIn());
+      } else {
+        inst.currentUser.listen(user => {
+          return deferred.resolve(user.isSignedIn())
+        });
+      }
+    });
+    return deferred.promise;
+  };
 }
